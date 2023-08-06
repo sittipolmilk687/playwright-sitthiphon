b507d6dbd964fc01bd7d4fe11e8c5192957bdcbb
sittipolmilk <sittipolmilk687@gmail.com>
2023-08-07 00:07:46 +0700

    delete comment xode

--- tests/e-tax_product.spec.ts
+++ tests/e-tax_product.spec.ts
@@ -7,9 +7,7 @@ test('test', async ({ page }) => {
   let mouse = page.locator("text=เลือกจำนวนใบกำกับภาษีที่คุณออก/เดือน");
   await mouse.scrollIntoViewIfNeeded();
   await page.waitForTimeout(1000);
-  // await page.locator('div').filter({ hasText: 'เลือกจำนวนใบกำกับภาษีที่คุณออก/เดือนBelow 1,5001,501 - 10,00010,001 - 100,000Mor' }).nth(3).click();
   await page.getByRole('combobox').selectOption('100000');
-  // await expect(page.getByText('CALCULATE')).toBeVisible();
   await expect(page.getByText('2,202,000 ฿')).toBeVisible();
   await expect(page.getByText('95,000฿')).toBeVisible();
 
